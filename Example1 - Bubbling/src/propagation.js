20dca09ee03da4df35126642f5236c90770369a6
haru <hyunnyta@hotmail.com>
2026-08-28 16:06:59 +0900

    주석추가

--- Example1 - Bubbling/src/propagation.js
+++ Example1 - Bubbling/src/propagation.js
@@ -188,3 +188,70 @@ if (document.readyState === 'loading') {
 } else {
     propagationBubbling();
 }
+
+
+  const cardBox = document.querySelector('.card-box');
+  const cardHeader = document.querySelector('.card-header span');
+  const displayConts = document.querySelector('.display-content');
+  const cardFooter = document.querySelector('.card-footer');
+  const refresh = document.querySelector('.refresh');
+  const option = document.querySelector('.option');
+  const msgDiv = document.querySelector('.clicked-msg p');
+  function msgAp(el) {
+      var msgSpan = document.createElement('span');
+      msgSpan.innerHTML = el;
+      msgDiv.appendChild(msgSpan)
+  }
+ function cardBoxE() {
+  msgAp("최고 하위요소인 카드박스가 클릭됨");
+}
+function cardHeaderE() {
+  msgAp("Header link 영역 클릭됨");
+}
+function displayContsE() {
+  msgAp("Display Content 영역 클릭됨");
+}
+function refreshE() {
+  msgAp("fresh 버튼 클릭됨");
+}
+function optionE() {
+  msgAp("option 버튼 클릭됨");
+}
+function cardFooterE() {
+  msgAp("cardFooter 버튼 클릭됨");
+}
+  cardBox.addEventListener('click', cardBoxE);
+  cardHeader.addEventListener('click', cardHeaderE);
+  displayConts.addEventListener('click', displayContsE);
+  refresh.addEventListener('click', refreshE);
+  option.addEventListener('click', optionE);
+  cardFooter.addEventListener('click', cardFooterE);
+
+// const cardBoxes = document.querySelectorAll('.card-box');
+// for(const cardBox of cardBoxes) {
+//   function bubblingProps() {
+//     var clickTargets = cardBox.querySelectorAll('button', 'a');
+//     for(const clickTarget of clickTargets) {
+//       clickTarget.addEventListener('click', function(e) {
+//         e.stopPropagation();
+//       })
+//     }
+//     var valueTargets = cardBox.querySelectorAll('.value');
+//     for(const valueTarget of valueTargets) {
+//       if(valueTarget.hasChildNodes('.showBalance')) {
+//         valueTarget.addEventListener('click', function(ele) {
+//           ele.stopPropagation();
+//         })
+//       }
+//     }
+//   }
+//   bubblingProps();
+//   var config = {attributes: true, childList: true, subtree: true}
+//   var observer = new MutationObserver(function(mutations) {
+//     mutations.forEach(function(mutation){
+//       bubblingProps();
+//       observer.disconnect();
+//     })
+//   })
+//   observe.observe(cardBox, config);
+// }
